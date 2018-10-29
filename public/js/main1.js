a0399ce130e50dc95440f394135cab1679ff3b46
feli <felromero9@gmail.com>
2018-10-29 16:15:59 -0300

    commit inicial

---
+++ public/js/main1.js
@@ -0,0 +1,65 @@
+// href of the buttums
+
+$('#calendar').click(function() {
+window.location.href ='calendar.html'
+});
+
+$('#location').click(function() {
+  window.location.href ='location.html'
+  });
+
+$('#results').click(function() {
+    window.location.href ='results.html'
+  });
+
+$('#contact').click(function() {
+  window.location.href ='contact.html'
+  });
+
+  // href of the calendar
+$('#ajKat').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/24+W+Walton+St,+Chicago,+IL+60610,+EE.+UU./@41.9002971,-87.6312384,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd34e07f6bac3:0x68a82e5d59952c86!8m2!3d41.9002931!4d-87.6290497'
+})
+$('#ajKat2').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/24+W+Walton+St,+Chicago,+IL+60610,+EE.+UU./@41.9002971,-87.6312384,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd34e07f6bac3:0x68a82e5d59952c86!8m2!3d41.9002931!4d-87.6290497'
+})
+
+$('#ajKat3').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/24+W+Walton+St,+Chicago,+IL+60610,+EE.+UU./@41.9002971,-87.6312384,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd34e07f6bac3:0x68a82e5d59952c86!8m2!3d41.9002931!4d-87.6290497'
+})
+
+
+$('#green').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/1734+N+Orleans+St,+Chicago,+IL+60614,+EE.+UU./@41.9133713,-87.6417829,14.75z/data=!4m5!3m4!1s0x880fd34073f306a3:0x9e1726bbf8f23f0e!8m2!3d41.9138035!4d-87.63784'
+})
+$('#green2').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/1734+N+Orleans+St,+Chicago,+IL+60614,+EE.+UU./@41.9133713,-87.6417829,14.75z/data=!4m5!3m4!1s0x880fd34073f306a3:0x9e1726bbf8f23f0e!8m2!3d41.9138035!4d-87.63784'
+})
+
+$('#marjorie').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/2625+N+Orchard+St,+Chicago,+IL+60614,+EE.+UU./@41.9295843,-87.648085,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd30f2630e551:0x3e719e44a5cef714!8m2!3d41.9295803!4d-87.6458963'
+})
+
+
+$('#north').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/1409+N+Ogden+Ave,+Chicago,+IL+60610,+EE.+UU./@41.9071287,-87.6483559,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd33af13a8945:0xb6ad1ec2b6f379ba!8m2!3d41.9071247!4d-87.6461672'
+})
+
+$('#north').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/1409+N+Ogden+Ave,+Chicago,+IL+60610,+EE.+UU./@41.9071287,-87.6483559,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd33af13a8945:0xb6ad1ec2b6f379ba!8m2!3d41.9071247!4d-87.6461672'
+})
+$('#north2').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/1409+N+Ogden+Ave,+Chicago,+IL+60610,+EE.+UU./@41.9071287,-87.6483559,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd33af13a8945:0xb6ad1ec2b6f379ba!8m2!3d41.9071247!4d-87.6461672'
+})
+
+$('#south').click(function(){
+  window.location.href='https://www.google.com.ar/maps/place/2101+N+Fremont+St,+Chicago,+IL+60614,+EE.+UU./@41.9197793,-87.6535579,17z/data=!3m1!4b1!4m5!3m4!1s0x880fd3196fb41dc7:0x970be7f7d6336df5!8m2!3d41.9197753!4d-87.6513692'
+})
+
+$('#howard').click(function(){
+  window.location.href='https://www.google.com/maps/place/Howard+A+Yeager+Elementary/@41.923261,-87.662921,13z/data=!4m5!3m4!1s0x0:0xb6cb22b2f0358874!8m2!3d41.923261!4d-87.6629215?hl=es-MX'
+})
+
+$('#howard2').click(function(){
+  window.location.href='https://www.google.com/maps/place/Howard+A+Yeager+Elementary/@41.923261,-87.662921,13z/data=!4m5!3m4!1s0x0:0xb6cb22b2f0358874!8m2!3d41.923261!4d-87.6629215?hl=es-MX'
+})
---
+++ public/js/mainfull.js
@@ -0,0 +1,41 @@
+$('#calendar').click(function() {
+  //window.location.href ='calendar.html'
+  $('#indexx').hide();
+  $('#calendary').show();
+  });
+  
+$('#location').click(function() {
+   // window.location.href ='location.html'
+  $('#indexx').hide();
+  $('#locationy').show();
+  });
+  
+$('#resultsbuttum').click(function() {
+   //   window.location.href ='results.html'
+  $('#indexx').hide();
+  $('#resulty').show();
+  });
+  
+$('#contactbutum').click(function() {
+   // window.location.href ='contact.html'
+  $('#indexx').hide();
+  $('#contaty').show();
+  });
+
+$('#forobuttum').click(function(){
+  //window.location.href = '#foros'
+  $('#indexx').hide();
+  $('#foros').show();
+})
+
+$('#ch1').click(function(){
+ // window.location.href = '#forotest'
+  $('#calendary').hide();
+  $('#foros').show();
+})
+
+$('#ch2').click(function(){
+  // window.location.href = '#forotest'
+   $('#calendary').hide();
+   $('#foros').show();
+ })
